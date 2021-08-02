bc01a398ecef5080b620f8e1c6c9cd4c5847e8ae
Philip Rehberger <me@philiprehberger.com>
2021-08-02 16:09:30 -0500

    Release v0.1.5

---
+++ tests/test_import.py
@@ -0,0 +1,7 @@
+"""Basic import test."""
+
+
+def test_import():
+    """Verify the package can be imported."""
+    import philiprehberger_func_timeout
+    assert hasattr(philiprehberger_func_timeout, "__name__") or True
